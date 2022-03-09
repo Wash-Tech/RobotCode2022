477d1c0f18c5c7497d359534ed936d7eb9c439eb
MikeZoua <98337898+MikeZoua@users.noreply.github.com>
2022-03-09 14:38:10 -0600

    Added Ball Storage Conveyor Spin
    -Button holding b spins at a speed of 0.5

--- src/main/java/frc/robot/commands/SpinConveyor.java
+++ src/main/java/frc/robot/commands/SpinConveyor.java
@@ -54,11 +54,13 @@ public class SpinConveyor extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_ballStorage.spinConveyor(0.5);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        m_ballStorage.spinConveyor(0);
     }
 
     // Returns true when the command should end.
