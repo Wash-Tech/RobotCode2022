0c4f020ff6c0b1600addbd32a942ccef4db19a54
MikeZoua <98337898+MikeZoua@users.noreply.github.com>
2022-03-16 15:49:05 -0500

    Fixed DriveTrain

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -53,25 +53,25 @@ private AnalogGyro analogGyro1;
 rightRearMC = new CANSparkMax(2, MotorType.kBrushless);
  
    
-rightRearMC.setInverted(false);
+rightRearMC.setInverted(true);
 rightRearMC.setIdleMode(IdleMode.kCoast);
   
 
 rightFrontMC = new CANSparkMax(3, MotorType.kBrushless);
  
-   
-rightFrontMC.setInverted(false);
+
+rightFrontMC.setInverted(true);
 rightFrontMC.setIdleMode(IdleMode.kCoast);
   
 
-leftRearMC = new CANSparkMax(4, MotorType.kBrushless);
+leftRearMC = new CANSparkMax(5, MotorType.kBrushless);
  
    
 leftRearMC.setInverted(false);
 leftRearMC.setIdleMode(IdleMode.kCoast);
   
 
-leftFrontMC = new CANSparkMax(5, MotorType.kBrushless);
+leftFrontMC = new CANSparkMax(4, MotorType.kBrushless);
  
    
 leftFrontMC.setInverted(false);
